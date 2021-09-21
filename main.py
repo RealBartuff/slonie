82bee92c74a0480b28f6f7f101125349e2757961
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-09-21 19:56:41 +0200

    wagi przypisane do sloni

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from sympy.combinatorics.partitions import Partition
 from sympy.combinatorics.permutations import Permutation
 
 with open("zadanie_B/slo1.in") as file:
-    N = file.readline()
+    N = int(file.readline())
     wagi = list(map(int, file.readline().split(" ")))
-    a = Permutation(list(map(int, file.readline().split(" "))))
-    b = Permutation(list(map(int, file.readline().split(" "))))
+    a = list(map(int, file.readline().split(" ")))
+    b = list(map(int, file.readline().split(" ")))
+    slow = dict(zip(wagi, a))
