575a156f39b0dc1e36115d91e3f2d426dca4e1ee
Bartłomiej Dębowski <bartek.993@gmail.com>
2021-10-16 15:14:44 +0200

    Prawidlowe rozwiazanie w main

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+def main():
+    n = int(input())
+    m = list(map(int, input().split(' ')))
+    a = list(map(lambda x: int(x) - 1, input().split(' ')))
+    b = list(map(lambda x: int(x) - 1, input().split(' ')))
+    p = [0 for _ in range(n)]
+    for i in range(n):
+        p[b[i]] = a[i]
+    C = []
+    vis = [False for _ in range(n)]
+    for i in range(n):
+        if not vis[i]:
+            c = [i]
+            vis[i] = True
+            x = p[i]
+            while vis[x] is not True:
+                vis[x] = True
+                c.append(x)
+                x = p[x]
+            C.append(c)
+    mass_sum = [sum([m[s] for s in c]) for c in C]
+    min_c = [min([m[s] for s in c]) for c in C]
+    min_all = min(m)
+    w = 0
+    for ci, c in enumerate(C):
+        met1 = mass_sum[ci] + (len(c) - 2) * min_c[ci]
+        met2 = mass_sum[ci] + min_c[ci] + (len(c) + 1) * min_all
+        w += min(met1, met2)
+    print(w)
 
-from sympy.combinatorics.partitions import Partition
-from sympy.combinatorics.permutations import Permutation
 
-with open("zadanie_B/slo1.in") as file:
-    N = int(file.readline())
-    wagi = list(map(int, file.readline().split(" ")))
-    a = list(map(int, file.readline().split(" ")))
-    b = list(map(int, file.readline().split(" ")))
-    slow = dict(zip(wagi, a))
+if __name__ == '__main__':
+    main()
--- main2.py
+++ main2.py
@@ -9,47 +9,41 @@ dl = 0
 suma = 0
 wynik = 0
 minc = 0
+a = 0
+nr = 0
 
+n = 10
+wagi = [3015, 4728, 4802, 4361, 135, 4444, 4313, 1413, 4581, 546]
+orig = [3, 10, 1, 8, 9, 4, 2, 7, 6, 5]
+perm = [4, 9, 5, 3, 1, 6, 10, 7, 8, 2]
+for a in range(0, (int(n) - 1) + 1):
+    if int(wagi[a - 1]) < minw:
+        minw = wagi[a - 1]
+for a in range(0, (int(n) - 1) + 1):
+    orig[a - 1] -= 1
+for a in range(0, (int(n) - 1) + 1):
+    for nr in perm:
+        nr -= 1
+        perm[int(nr) - 1] = orig[a - 1]
+        vis[a - 1] = False
 
-
-with open("zadanie_B/slo1.in") as file:
-    a = 0
-    nr = 0
-    n = file.readline()
-    wagi = file.readline().split(" ")
-    wagi = list(map(int, wagi))
-    orig = file.readline().split(" ")
-    orig = list(map(int, orig))
-    perm = file.readline().split(" ")
-    perm = list(map(int, perm))
-    for a in range(0, (int(n) - 1) + 1):
-        if int(wagi[a - 1]) < minw:
-            minw = wagi[a - 1]
-    for a in range(0, (int(n) - 1) + 1):
-        orig[a - 1] -= 1
-    for a in range(0, (int(n) - 1) + 1):
-        for nr in perm:
-            nr -= 1
-            perm[int(nr) - 1] = orig[a - 1]
-            vis[a - 1] = False
-
-    for pocz in range(0, (int(n) - 1) + 1):
-        if not vis[pocz]:
-            minc = inf
-            suma = 0
-            cur = pocz
-            dl = 0
-            while True:
-                if wagi[cur] < minc:
-                    minc = wagi[cur]
-                suma = suma + int(wagi[cur])
-                cur = perm[cur]
-                vis[int(cur)] = True
-                dl += 1
-                if cur == pocz:
-                    break
-            if suma + (dl - 2) * minc < suma + minc + (dl + 1) * minw:
-                wynik = wynik + suma + (dl + 2) * minc
-            else:
-                wynik = wynik + suma + minc + (dl + 1) * minw
-    print(wynik)
+for pocz in range(0, (int(n) - 1) + 1):
+    if not vis[pocz]:
+        minc = inf
+        suma = 0
+        cur = pocz
+        dl = 0
+        while True:
+            if wagi[cur] < minc:
+                minc = wagi[cur]
+            suma = suma + int(wagi[cur])
+            cur = perm[cur]
+            vis[int(cur)] = True
+            dl += 1
+            if cur == pocz:
+                break
+        if suma + (dl - 2) * minc < suma + minc + (dl + 1) * minw:
+            wynik = wynik + suma + (dl + 2) * minc
+    else:
+        wynik = wynik + suma + minc + (dl + 1) * minw
+print(wynik)
